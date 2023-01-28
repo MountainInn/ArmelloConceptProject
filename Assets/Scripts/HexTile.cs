a764f8ff2666e7ee7c2df35b77e032f3041a83f5
Mountain <blckclst@gmail.com>
2023-01-29 04:31:47 +0500

    Add highlighting when mouse is over HexTile

--- Assets/Scripts/HexTile.cs
+++ Assets/Scripts/HexTile.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Mirror;
 
-public class HexTile : NetworkBehaviour, IPointerClickHandler
+public class HexTile : NetworkBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     Vector2Int coordinates;
+    private SpriteRenderer spriteRenderer;
+    private Color baseColor, highlightColor;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        baseColor = spriteRenderer.color;
+        highlightColor = baseColor * 1.1f;
+    }
 
     public void Initialize(Vector2Int coordinates)
     {
@@ -14,5 +23,15 @@ public class HexTile : NetworkBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Hex!");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        spriteRenderer.color = highlightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        spriteRenderer.color = baseColor;
     }
 }

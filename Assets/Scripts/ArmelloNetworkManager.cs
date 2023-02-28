f889c329d6f76e33be0b45373de77077f95fe5c3
Mountain <blckclst@gmail.com>
2023-02-28 13:13:23 +0500

    Start separating game scenes

--- Assets/Scripts/ArmelloNetworkManager.cs
+++ Assets/Scripts/ArmelloNetworkManager.cs
@@ -1,31 +1,43 @@
 using Mirror;
 using MountainInn;
+using System;
 using System.Linq;
+using UnityEngine;
 using Zenject;
 
 public class ArmelloNetworkManager : NetworkManager
 {
+    private const string GAME_SCENE_NAME = "Game Scene";
     private TurnSystem turnSystem;
+    private GameStartNotifier gameStartNotifier;
+
+    public event System.Action onStopClient;
+
+    public override void Awake()
+    {
+        base.Awake();
+    }
 
     public override void OnStartServer()
     {
         base.OnStartServer();
 
-        var turnResolver = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Turn Resolver"));
+        var turnResolver = InstantiatePrefab<TurnSystem>("Turn Resolver");
+        turnSystem = InstantiatePrefab<TurnSystem>("Turn System");
+        gameStartNotifier = InstantiatePrefab<GameStartNotifier>("Game Start Notifier");
+
         NetworkServer.Spawn(turnResolver.gameObject);
-
-        turnSystem =
-            Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Turn System"))
-            .GetComponent<TurnSystem>();
-
         NetworkServer.Spawn(turnSystem.gameObject);
+        NetworkServer.Spawn(gameStartNotifier.gameObject);
+
+        FindObjectOfType<EOSLobbyUI>()
+            .onStartGameButtonClicked += () => ServerChangeScene(GAME_SCENE_NAME);
     }
+
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
-        Player player =
-            Instantiate(playerPrefab)
-            .GetComponent<Player>();
+        Player player = Instantiate(playerPrefab).GetComponent<Player>();
 
         player.name = $"Player [connId={conn.connectionId}]";
         NetworkServer.AddPlayerForConnection(conn, player.gameObject);
@@ -43,6 +55,21 @@ public class ArmelloNetworkManager : NetworkManager
         turnSystem.UnregisterPlayer(disconnectedPlayer);
 
         base.OnServerDisconnect(conn);
+    }
+
+    public override void OnStopClient()
+    {
+        onStopClient?.Invoke();
+    }
+
+    private T InstantiatePrefab<T>(string name)
+    {
+        return Instantiate(GetPrefab(name)).GetComponent<T>();
+    }
+
+    private GameObject GetPrefab(string name)
+    {
+        return spawnPrefabs.Find(prefab => prefab.name == name);
     }
 
 }

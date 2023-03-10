27625d80f7c5a08811ca971e52f5441680c41cd0
Mountain <blckclst@gmail.com>
2023-03-11 03:39:05 +0500

    Add OutcomUI made with UIBuilder

---
+++ Assets/Scripts/OutcomeUI.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OutcomeUI : MonoBehaviour
+{
+    VisualElement root;
+    Image outcomeBackround;
+    Label outcomeLabel;
+    Button leaveButton;
+
+    private void Awake()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+        outcomeBackround = root.Q<Image>();
+        outcomeLabel = root.Q<Label>("OutcomeLabel");
+        leaveButton = root.Q<Button>("LeaveButton");
+
+
+        leaveButton.clicked += LeaveGame;
+    }
+
+    private void Start()
+    {
+        outcomeBackround.ToggleInClassList("fade-in");
+    }
+
+    private void LeaveGame()
+    {
+        NetworkClient.Disconnect();
+    }
+
+    public void ShowLoss()
+    {
+        outcomeLabel.text = "You lose!";
+    }
+
+    public void ShowVictory()
+    {
+        outcomeLabel.text = "You win!";
+    }
+}
